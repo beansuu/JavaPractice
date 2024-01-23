54d3c81a640307cac73289651bac6271a882f5fd
beansuu <mauno.talli@hotmail.com>
2024-01-23 20:09:58 +0200

    flux from stream

---
+++ reactor/src/main/java/com/rp/sec02/Lec04FluxFromStream.java
@@ -0,0 +1,28 @@
+package com.rp.sec02;
+
+import com.rp.courseutil.Util;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Lec04FluxFromStream {
+
+    public static void main(String[] args) {
+
+
+        List<Integer> list = List.of(1, 2, 3, 4, 5);
+        Stream<Integer> stream = list.stream();
+
+
+        Flux<Integer> integerFlux = Flux.fromStream(() -> list.stream());
+
+        integerFlux
+                .subscribe(
+                        Util.onNext(),
+                        Util.onError(),
+                        Util.onComplete()
+                );
+    }
+
+}
