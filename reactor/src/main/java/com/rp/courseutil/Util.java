12c21de1c55318cdf42cdb3eb2edae71a9479a39
beansuu <mauno.talli@hotmail.com>
2024-01-23 14:48:47 +0200

    supplierrefactoring

--- reactor/src/main/java/com/rp/courseutil/Util.java
+++ reactor/src/main/java/com/rp/courseutil/Util.java
@@ -23,5 +23,14 @@ public class Util {
     public static Faker faker() {
         return FAKER;
     }
-}
+
+    public static void sleepSeconds(int seconds) {
+            try {
+                Thread.sleep(seconds * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 
