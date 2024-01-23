cea862ca02bde126cff26f4c07b559c137d442da
beansuu <mauno.talli@hotmail.com>
2024-01-24 00:12:28 +0200

    flux from mono

--- reactor/src/main/java/com/rp/sec02/Lec09FluxFromMono.java
+++ reactor/src/main/java/com/rp/sec02/Lec09FluxFromMono.java
@@ -8,14 +8,17 @@ public class Lec09FluxFromMono {
 
     public static void main(String[] args) {
 
-        Mono<String> mono = Mono.just("a");
+   /*     Mono<String> mono = Mono.just("a");
 
         Flux<String> flux = Flux.from(mono);
 
         flux.subscribe(Util.onNext());
     }
 
-    public static void doSomething(Flux<String> flux) {
-
+    */
+        Flux.range(1, 10)
+                .next() // 1
+                .filter(i -> i > 3)
+                .subscribe(Util.onNext(), Util.onError(), Util.onComplete());
     }
 }
