dceae1cc5d8ffcd187d974f8c004c84bc8d38069
beansuu <mauno.talli@hotmail.com>
2024-01-23 14:33:17 +0200

    callable

--- reactor/src/main/java/com/rp/sec01/Lex05MonoFromSupplier.java
+++ reactor/src/main/java/com/rp/sec01/Lex05MonoFromSupplier.java
@@ -3,6 +3,9 @@ package com.rp.sec01;
 import com.rp.courseutil.Util;
 import reactor.core.publisher.Mono;
 
+import java.util.concurrent.Callable;
+import java.util.function.Supplier;
+
 public class Lex05MonoFromSupplier {
 
     public static void main(String[] args) {
@@ -10,11 +13,17 @@ public class Lex05MonoFromSupplier {
         // only use if you have data already
        //  Mono<String> mono = Mono.just(getName());
 
+        Supplier<String> stringSupplier = () -> getName();
         Mono<String> mono = Mono.fromSupplier(() -> getName());
         mono.subscribe(
                 Util.onNext()
         );
 
+        Callable<String> stringCallable = () -> getName();
+        Mono.fromCallable(stringCallable)
+                .subscribe(
+                        Util.onNext()
+                );
     }
 
     private static String getName() {
