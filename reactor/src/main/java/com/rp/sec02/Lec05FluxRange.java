a2793def52833ead6d933955ede0d8f790718656
beansuu <mauno.talli@hotmail.com>
2024-01-23 22:21:34 +0200

    flux range

---
+++ reactor/src/main/java/com/rp/sec02/Lec05FluxRange.java
@@ -0,0 +1,17 @@
+package com.rp.sec02;
+
+import com.rp.courseutil.Util;
+import reactor.core.publisher.Flux;
+
+public class Lec05FluxRange {
+
+    public static void main(String[] args) {
+
+        Flux.range(1, 10)
+                .map(i -> Util.faker().name().fullName())
+                .subscribe(
+                        Util.onNext()
+                );
+
+    }
+}
