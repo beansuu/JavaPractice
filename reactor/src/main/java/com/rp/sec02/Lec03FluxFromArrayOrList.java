3b76799bdfccc7bf08f34aba48fba60737a3f881
beansuu <mauno.talli@hotmail.com>
2024-01-23 20:04:43 +0200

    flux from array or list

---
+++ reactor/src/main/java/com/rp/sec02/Lec03FluxFromArrayOrList.java
@@ -0,0 +1,14 @@
+package com.rp.sec02;
+
+import com.rp.courseutil.Util;
+import reactor.core.publisher.Flux;
+
+public class Lec03FluxFromArrayOrList {
+
+    public static void main(String[] args) {
+
+        Integer[] arr = {2, 5, 7, 8};
+        Flux.fromArray(arr)
+                .subscribe(Util.onNext());
+    }
+}
