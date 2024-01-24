ead065323d5698b522a7edb9c557008662111dac
beansuu <mauno.talli@hotmail.com>
2024-01-24 15:29:58 +0200

    creating a flux

--- reactor/src/main/java/com/rp/courseutil/Util.java
+++ reactor/src/main/java/com/rp/courseutil/Util.java
@@ -1,6 +1,7 @@
 package com.rp.courseutil;
 
 import com.github.javafaker.Faker;
+import org.reactivestreams.Subscriber;
 
 import java.util.function.Consumer;
 
@@ -31,6 +32,13 @@ public class Util {
                 e.printStackTrace();
             }
         }
+   public static Subscriber<Object> subscriber() {
+       return new DefaultSubscriber();
+    }
+
+    public static Subscriber<Object> subscriber(String name) {
+        return new DefaultSubscriber(name);
+      }
     }
 
 
